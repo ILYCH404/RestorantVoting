90522cd75ab63633c5b2a6933b4178e488446efd
Ilya Chukanov <ilyachuk77@gmail.com>
2022-04-01 10:42:49 +0300

    add restaurant field

--- src/main/java/com/example/restoranvoting/model/Meal.java
+++ src/main/java/com/example/restoranvoting/model/Meal.java
@@ -3,11 +3,10 @@ package com.example.restoranvoting.model;
 import lombok.*;
 import org.hibernate.validator.constraints.Range;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
@@ -17,24 +16,32 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(callSuper = true)
 public class Meal extends BaseEntity {
+
     @Column(name = "date_time")
     @NotNull
     private LocalDateTime dateTime;
+
     @Column(name = "description")
     @NotBlank
     private String description;
+
     @Column(name = "price")
     @Range(min = 100, max = 10000)
-    private int price;
+    private BigDecimal price;
+
     @Column(name = "calories")
     @Range(min = 10, max = 5000)
     private int calories;
 
-    public Meal(LocalDateTime dateTime, String description, int price, int calories) {
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    public Meal(LocalDateTime dateTime, String description, BigDecimal price, int calories) {
         this(null, dateTime, description, price, calories);
     }
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int price, int calories) {
+    public Meal(Integer id, LocalDateTime dateTime, String description, BigDecimal price, int calories) {
         super(id);
         this.dateTime = dateTime;
         this.description = description;
