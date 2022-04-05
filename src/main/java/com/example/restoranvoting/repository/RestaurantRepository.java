05bd3dec2e08aedcff0f49a6391541e714a3d1de
Ilya Chukanov <ilyachuk77@gmail.com>
2022-04-05 16:36:20 +0300

    add RestaurantsRestController

--- src/main/java/com/example/restoranvoting/repository/RestaurantRepository.java
+++ src/main/java/com/example/restoranvoting/repository/RestaurantRepository.java
@@ -2,5 +2,8 @@ package com.example.restoranvoting.repository;
 
 import com.example.restoranvoting.model.Restaurant;
 
+import java.util.Optional;
+
 public interface RestaurantRepository extends BaseRepository<Restaurant> {
+    Optional<Restaurant> findByDescription(String description);
 }
---
+++ src/main/java/com/example/restoranvoting/web/restaraunt/RestaurantsRestController.java
@@ -0,0 +1,65 @@
+package com.example.restoranvoting.web.restaraunt;
+
+import com.example.restoranvoting.model.Restaurant;
+import com.example.restoranvoting.repository.RestaurantRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.Objects;
+
+import static com.example.restoranvoting.util.validation.ValidationUtil.checkNew;
+
+
+@RestController
+@RequestMapping(value = RestaurantsRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
+public class RestaurantsRestController {
+    static final String REST_URL = "/admin/restaurants";
+
+    @Autowired
+    protected RestaurantRepository repository;
+
+    @GetMapping
+    public List<Restaurant> getAll() {
+        log.info("getAll");
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Restaurant> get(@PathVariable int id) {
+        log.info("get {}", id);
+        return ResponseEntity.of(Objects.requireNonNull(repository.findById(id)));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        log.info("delete {}", id);
+        repository.delete(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Restaurant> createWithLocation(@Valid @RequestBody Restaurant restaurant) {
+        log.info("create {}", restaurant);
+        checkNew(restaurant);
+        Restaurant created = repository.save(restaurant);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @GetMapping("/by-description")
+    public ResponseEntity<Restaurant> getByDescription(@RequestParam String description) {
+        log.info("getByDescription {}", description);
+        return ResponseEntity.of(repository.findByDescription(description));
+    }
+}
