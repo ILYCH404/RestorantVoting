5698dee86d4fa5af15da0db203d1c38a4334a4fb
Ilya Chukanov <ilyachuk77@gmail.com>
2022-04-05 19:41:39 +0300

    add MealRestController

---
+++ src/main/java/com/example/restoranvoting/web/meal/MealRestController.java
@@ -0,0 +1,60 @@
+package com.example.restoranvoting.web.meal;
+
+import com.example.restoranvoting.model.Meal;
+import com.example.restoranvoting.repository.MealRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+import static com.example.restoranvoting.util.validation.ValidationUtil.checkNew;
+
+@RestController
+@RequestMapping(value = MealRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
+// TODO: cache only most requested data!
+public class MealRestController {
+
+    static final String REST_URL = "/restaurant/meals";
+
+    @Autowired
+    protected MealRepository repository;
+
+    @GetMapping
+    public List<Meal> getAll() {
+        log.info("getAll");
+        return repository.findAll(Sort.by(Sort.Direction.ASC, "description"));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Meal> get(@PathVariable int id) {
+        log.info("get {}", id);
+        return ResponseEntity.of(repository.findById(id));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Meal> createWithLocation(@Valid @RequestBody Meal meal) {
+        log.info("create{}", meal);
+        checkNew(meal);
+        Meal created = repository.save(meal);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        log.info("delete {}", id);
+        repository.delete(id);
+    }
+}
