087fa134e00df7d5ac9688f37d26a10728fc752a
Ilya Chukanov <ilyachuk77@gmail.com>
2022-04-10 16:52:54 +0300

    upgrade vote to 1 method

--- src/main/java/com/example/restoranvoting/repository/VoteRepository.java
+++ src/main/java/com/example/restoranvoting/repository/VoteRepository.java
@@ -1,11 +1,17 @@
 package com.example.restoranvoting.repository;
 
+import com.example.restoranvoting.model.Restaurant;
 import com.example.restoranvoting.model.Vote;
-
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
-public interface VoteRepository extends BaseRepository<Vote>{
+public interface VoteRepository extends BaseRepository<Vote> {
 
-    Vote findByUser_IdAndRestaurant_Id(int user_id, int restaurant_id);
+    @Modifying
+    @Query("update Vote v set v.restaurant = ?1 where v.id = ?2")
+    void setRestaurant(Restaurant restaurant_id, int user_id);
+
+    boolean existsByUserId(int user_id);
 }
