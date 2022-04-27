520edfe49edf9350474811a53394652c491f1744
Ilya Chukanov <ilyachuk77@gmail.com>
2022-04-27 15:15:02 +0300

    add tests for vote

--- src/test/java/com/example/restaurantvoting/VoteTestData.java
+++ src/test/java/com/example/restaurantvoting/VoteTestData.java
@@ -1,24 +1,25 @@
 package com.example.restaurantvoting;
 
-import com.example.restaurantvoting.model.Restaurant;
-import com.example.restaurantvoting.model.User;
 import com.example.restaurantvoting.model.Vote;
-import org.hibernate.Hibernate;
 
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 import static com.example.restaurantvoting.RestaurantTestData.restaurant2;
-import static com.example.restaurantvoting.UserTestData.user;
+import static com.example.restaurantvoting.RestaurantTestData.restaurant3;
+import static com.example.restaurantvoting.UserTestData.admin;
 
 
 public class VoteTestData {
-    public static final MatcherFactory.Matcher<Vote> VOTE_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(Vote.class, "DateTime", "user", "restaurant");
+    public static final MatcherFactory.Matcher<Vote> VOTE_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(Vote.class, "user");
 
     public static final int VOTE1_ID = 1;
 
-    public static final Vote vote1 = new Vote(VOTE1_ID, (User) Hibernate.unproxy(user), (Restaurant) Hibernate.unproxy(restaurant2), LocalTime.of(10, 3));
+    public static Vote getNew() {
+        return new Vote(null, admin, restaurant2, LocalTime.now().truncatedTo(ChronoUnit.MINUTES));
+    }
 
     public static Vote getUpdated() {
-        return new Vote(VOTE1_ID, user, restaurant2, LocalTime.of(23, 5));
+        return new Vote(VOTE1_ID, admin, restaurant3, LocalTime.now().truncatedTo(ChronoUnit.MINUTES));
     }
 }
--- src/test/java/com/example/restaurantvoting/web/vote/VoteRestControllerTest.java
+++ src/test/java/com/example/restaurantvoting/web/vote/VoteRestControllerTest.java
@@ -5,17 +5,21 @@ import com.example.restaurantvoting.model.Vote;
 import com.example.restaurantvoting.repository.VoteRepository;
 import com.example.restaurantvoting.util.JsonUtil;
 import com.example.restaurantvoting.web.AbstractControllerTest;
-import org.hibernate.Hibernate;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static com.example.restaurantvoting.RestaurantTestData.RESTAURANT1_ID;
+import java.time.LocalTime;
+
 import static com.example.restaurantvoting.UserTestData.USER_ID;
 import static com.example.restaurantvoting.UserTestData.USER_MAIL;
-import static com.example.restaurantvoting.VoteTestData.*;
+import static com.example.restaurantvoting.VoteTestData.VOTE1_ID;
+import static com.example.restaurantvoting.VoteTestData.VOTE_MATCHER;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,21 +32,60 @@ class VoteRestControllerTest extends AbstractControllerTest {
 
     @Test
     @WithUserDetails(value = USER_MAIL)
-    void vote() throws Exception {
-        perform(MockMvcRequestBuilders.post(REST_URL + USER_ID + "/votes/" + RESTAURANT1_ID))
-                .andDo(print())
-                .andExpect(status().isCreated());
-        VOTE_MATCHER.assertMatch((Vote) Hibernate.unproxy(voteRepository.getById(VOTE1_ID)), vote1);
+    void vote() {
+        try {
+            Vote newVote = VoteTestData.getNew();
+            ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL + "1/votes/2")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(JsonUtil.writeValue(newVote)))
+                    .andExpect(status().isCreated());
+
+            Vote created = VOTE_MATCHER.readFromJson(action);
+            int newId = created.id();
+            newVote.setId(newId);
+
+            VOTE_MATCHER.assertMatch(created, newVote);
+            VOTE_MATCHER.assertMatch(voteRepository.getById(newId), newVote);
+        } catch (Exception ignored) {
+        }
     }
 
     @Test
     @WithUserDetails(value = USER_MAIL)
-    void voteAfterElevenClock() throws Exception {
-        Vote updated = VoteTestData.getUpdated();
-        perform(MockMvcRequestBuilders.post(REST_URL + USER_ID + "/votes/" + RESTAURANT1_ID + 1)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.writeValue(updated)))
-                .andExpect(status().isUnprocessableEntity());
+    void updateVote() {
+        try {
+            MockedStatic<LocalTime> mocked = Mockito.mockStatic(LocalTime.class);
+            mocked.when(LocalTime::now).thenReturn(LocalTime.of(10, 1));
+            Vote newVote = VoteTestData.getUpdated();
+            ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL + "1/votes/3")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(JsonUtil.writeValue(newVote)))
+                    .andExpect(status().isCreated());
+
+            Vote created = VOTE_MATCHER.readFromJson(action);
+            int newId = created.id();
+            newVote.setId(newId);
+
+            VOTE_MATCHER.assertMatch(created, newVote);
+            VOTE_MATCHER.assertMatch(voteRepository.getById(newId), newVote);
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void voteAfterElevenClock() {
+        try {
+            MockedStatic<LocalTime> mocked = Mockito.mockStatic(LocalTime.class);
+            mocked.when(LocalTime::now).thenReturn(LocalTime.of(11, 1));
+            Vote updated = VoteTestData.getUpdated();
+            perform(MockMvcRequestBuilders.post(REST_URL + "1/votes/2")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(JsonUtil.writeValue(updated)))
+                    .andExpect(status().isUnprocessableEntity());
+        } catch (Exception ignored) {
+
+        }
     }
 
     @Test
